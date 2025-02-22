84e9b093d8514f56592b4bcfe8891419e2ea4120
chemicola <jose741983@gmail.com>
2025-02-22 15:45:24 +0100

    Combinar datos relevantes

---
+++ recomendador_netflix.py
@@ -0,0 +1,24 @@
+# 1. Importamos las librerías. 
+# pandas: Se usa para cargar y manipular datos en formato tabular
+# numpy: Es útil para trabajar con matrices y operaciones numéricas.
+import pandas as pd
+import numpy as np
+
+
+# 2. Cargar datos
+from sklearn.feature_extraction.text import TfidfVectorizer 
+# TfidfVectorizer: Es una herramienta para convertir texto en números (vectores) que la computadora puede entender.
+from sklearn.metrics.pairwise import cosine_similarity
+# cosine_similarity: Es una técnica para medir qué tan similares son dos cosas basadas en sus características numéricas.
+
+datos = pd.read_csv("netflix_titles.csv") # Cargamos nuestro archivo CSV.
+print(datos.head()) # Muestra las primeras 5 filas.
+
+# 3. Combinar características relevantes. Crear una función que toma una fila de la tabla 
+# y combina varias columnas en una sola cadena de texto.
+def combinar_caracteristicas(fila):
+    return f"{fila['director']} {fila['cast']} {fila['listed_in']} {fila['description']}"
+datos["características"] = datos.apply(combinar_caracteristicas, axis=1)
+
+
+
