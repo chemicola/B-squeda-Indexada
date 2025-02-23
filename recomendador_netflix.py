f0949922660a8c31e4fdc875f5c712d5ddfcc4d7
chemicola <jose741983@gmail.com>
2025-02-23 12:59:26 +0100

    Función de recomendación y ejemplo de uso

--- recomendador_netflix.py
+++ recomendador_netflix.py
@@ -26,3 +26,23 @@ matriz_tdfif = tfdif.fit_transform(datos["características"]) # Convertimos la c
 
 # 5. Calcular la similitud entre películas.
 similitud_coseno = cosine_similarity(matriz_tdfif, matriz_tdfif) # Resultado: Una tabla (matriz) donde cada número indica qué tan similares son dos películas.
+
+# 6. Crear función para recomendar películas.
+def recomendar_titulo(titulo, similitud_coseno=similitud_coseno):
+    # Encontrar el índice del título.
+    indice = datos[datos['title'] == titulo].index[0] # uscamos el número de fila (índice) de la película que queremos usar como referencia.
+    
+    # Obtenemos una lista de todas las películas con su puntuación de similitud respecto a la película de referencia.
+    puntuaciones_similitud = list(enumerate(similitud_coseno[indice])) 
+    
+    # Ordenar las películas de mayor a menor por similitud.
+    puntuaciones_similitud = sorted(puntuaciones_similitud, key=lambda x: x[1], reverse=True)
+    
+    # Obtener los índices de las 5 películas más similares (excluyendo la misma película)
+    indices_similares = [i[0] for i in puntuaciones_similitud[1:6]]
+    
+    # Devolver los títulos recomendados
+    return datos['title'].iloc[indices_similares]
+
+# Ejemplo de uso
+print(recomendar_titulo("Inception"))
